5ba49f6982e5adc213b5c474211640ee4a86e58a
Rafael Barboza <barboza.cardoso@gmail.com>
2014-05-26 18:33:00 -0300

    Add the amazon region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,8 @@ CarrierWave.configure do |config|
       host: 's3.amazonaws.com',
       endpoint: 'https://s3.amazonaws.com',
       aws_access_key_id: Configuration[:aws_access_key],
-      aws_secret_access_key: Configuration[:aws_secret_key]
+      aws_secret_access_key: Configuration[:aws_secret_key],
+      region: 'us-west-2'
     }
     config.fog_directory  = Configuration[:aws_bucket]
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
